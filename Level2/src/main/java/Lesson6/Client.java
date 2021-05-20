988c71ba76bce12e9a440054094c79d2ccacf0d6
DaniilRakov <daniilrakov@mail.ru>
2021-05-20 18:11:16 +0300

    Сделал консольное общение между клиентом и сервером

--- Level2/src/main/java/Lesson6/Client.java
+++ Level2/src/main/java/Lesson6/Client.java
@@ -1,26 +1,18 @@
 package Lesson6;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class Client extends JFrame {
-    private final String SERVER_ADDRESS = "localhost";
-    private final int SERVER_PORT = 8189;
-
-    private JTextField msgInputField;
-    private JTextArea chatArea;
+public class Client {
 
     private Socket socket;
     private DataInputStream inputStream;
     private DataOutputStream outputStream;
+    private Scanner scanner;
 
     public Client() {
         try {
@@ -28,36 +20,50 @@ public class Client extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        prepareGUI();
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new Client();
-            }
-        });
+        new Client();
     }
 
     public void openConnection() throws IOException {
-        socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+        try {
+            socket = new Socket(Constants.SERVER_ADDRESS, Constants.SERVER_PORT);
+            System.out.println("Подключение успешно");
+        } catch (Exception ex) {
+            System.out.println("Не удалось подключиться к серверу");
+            return;
+        }
         inputStream = new DataInputStream(socket.getInputStream());
         outputStream = new DataOutputStream(socket.getOutputStream());
+        scanner = new Scanner(System.in);
+
+        // поток отправки сообщений
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true)
+                    if (scanner.hasNextLine()) {
+                        sendMessage();
+                    }
+            }
+        }).start();
+
+        // поток считывания сообщений
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     while (true) {
                         String strFromServer = inputStream.readUTF();
+                        System.out.println("Сервер: " + strFromServer);
                         if (strFromServer.equalsIgnoreCase("/end")) {
+                            closeConnection();
                             break;
                         }
-                        chatArea.append(strFromServer);
-                        chatArea.append("\n");
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
                 }
             }
         }).start();
@@ -79,68 +85,16 @@ public class Client extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        scanner.close();
     }
 
     public void sendMessage() {
-        if (!msgInputField.getText().trim().isEmpty()) {
-            try {
-                outputStream.writeUTF(msgInputField.getText());
-                msgInputField.setText("");
-                msgInputField.grabFocus();
-            } catch (IOException e) {
-                e.printStackTrace();
-                JOptionPane.showMessageDialog(null, "Ошибка отправки сообщения");
-            }
+        try {
+            outputStream.writeUTF(scanner.nextLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Ошибка отправки сообщения");
         }
-    }
-
-    public void prepareGUI() {
-        // Параметры окна
-        setBounds(600, 300, 500, 500);
-        setTitle("Клиент");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        // Текстовое поле для вывода сообщений
-        chatArea = new JTextArea();
-        chatArea.setEditable(false);
-        chatArea.setLineWrap(true);
-        add(new JScrollPane(chatArea), BorderLayout.CENTER);
-
-        // Нижняя панель с полем для ввода сообщений и кнопкой отправки сообщений
-        JPanel bottomPanel = new JPanel(new BorderLayout());
-        JButton btnSendMsg = new JButton("Отправить");
-        bottomPanel.add(btnSendMsg, BorderLayout.EAST);
-        msgInputField = new JTextField();
-        add(bottomPanel, BorderLayout.SOUTH);
-        bottomPanel.add(msgInputField, BorderLayout.CENTER);
-        btnSendMsg.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sendMessage();
-            }
-        });
-        msgInputField.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sendMessage();
-            }
-        });
-
-        // Действие на закрытие окна
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                super.windowClosing(e);
-                try {
-                    outputStream.writeUTF("/end");
-                    closeConnection();
-                } catch (IOException exc) {
-                    exc.printStackTrace();
-                }
-            }
-        });
-
-        setVisible(true);
     }
 }
 
--- Level2/src/main/java/Lesson6/Server.java
+++ Level2/src/main/java/Lesson6/Server.java
@@ -5,24 +5,51 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
     public static void main(String[] args) {
         Socket socket;
-        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+        try (ServerSocket serverSocket = new ServerSocket(Constants.SERVER_PORT)) {
             System.out.println("Сервер запущен, ожидаем подключения...");
             socket = serverSocket.accept();
             System.out.println("Клиент подключился");
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            while (true) {
-                String str = in.readUTF();
-                if (str.equals("/end")) {
-                    break;
+            final DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            final DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            final Scanner scanner = new Scanner(System.in);
+
+            // поток считывания сообщений
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String str = inputStream.readUTF();
+                            System.out.println("Клиент: " + str);
+                            if (str.equals("/end")) {
+                                break;
+                            }
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
                 }
-                out.writeUTF("Эхо: " + str);
-            }
+            }).start();
+
+            // поток отправки сообщений
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true)
+                        if (scanner.hasNextLine())
+                            try {
+                                outputStream.writeUTF(scanner.nextLine());
+                            } catch (IOException exception) {
+                                exception.printStackTrace();
+                            }
+                }
+            }).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
