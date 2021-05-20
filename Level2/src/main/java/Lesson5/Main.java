fd4ee47e0886c476d98ebcb223b401cdfd81e3dc
DaniilRakov <daniilrakov@mail.ru>
2021-05-20 16:39:52 +0300

    Исправил замечания

--- Level2/src/main/java/Lesson5/Main.java
+++ Level2/src/main/java/Lesson5/Main.java
@@ -25,38 +25,40 @@ public class Main {
     }
 
     private static void secondMethod() throws InterruptedException {
-        float[] fullArray = new float[SIZE];
+        final float[] fullArray = new float[SIZE];
         Arrays.fill(fullArray, 1);
         long timeStart = System.currentTimeMillis();
 
-        final float[] firstArray = new float[HALF_OF_SIZE];
-        final float[] secondArray = new float[HALF_OF_SIZE];
-        System.arraycopy(fullArray, 0, firstArray, 0, HALF_OF_SIZE);
-        System.arraycopy(fullArray, HALF_OF_SIZE, secondArray, 0, HALF_OF_SIZE);
-
         Thread thread1 = new Thread(new Runnable() {
             public void run() {
+                final float[] firstArray = new float[HALF_OF_SIZE];
+                System.arraycopy(fullArray, 0, firstArray, 0, HALF_OF_SIZE);
+
                 for (int i = 0; i < HALF_OF_SIZE; i++)
-                    firstArray[i] = (float) (firstArray[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                    firstArray[i] = (float) (firstArray[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
+                            Math.cos(0.4f + i / 2));
+
+                System.arraycopy(firstArray, 0, fullArray, 0, HALF_OF_SIZE);
             }
         });
 
         Thread thread2 = new Thread(new Runnable() {
             public void run() {
+                final float[] secondArray = new float[HALF_OF_SIZE];
+                System.arraycopy(fullArray, HALF_OF_SIZE, secondArray, 0, HALF_OF_SIZE);
+
                 for (int i = HALF_OF_SIZE; i < SIZE; i++)
                     secondArray[i - HALF_OF_SIZE] = (float) (secondArray[i - HALF_OF_SIZE] * Math.sin(0.2f + i / 5) *
                             Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+                System.arraycopy(secondArray, 0, fullArray, HALF_OF_SIZE, HALF_OF_SIZE);
             }
         });
-
 
         thread1.start();
         thread2.start();
         thread1.join();
         thread2.join();
-
-        System.arraycopy(firstArray, 0, fullArray, 0, HALF_OF_SIZE);
-        System.arraycopy(secondArray, 0, fullArray, HALF_OF_SIZE, HALF_OF_SIZE);
 
         long timeEnd = System.currentTimeMillis();
         System.out.println("Время работы второго метода: " + (timeEnd - timeStart));
